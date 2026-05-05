f4cd9cb97f127449dd448e234f93f0ba83799db3
Steeven Toala <steevenpfb@gmail.com>
2026-05-05 17:58:59 -0500

    se agrega la actualizacion del usuario del cliente

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,11 +1,39 @@
+import { IsBoolean, IsDateString, IsOptional, IsString } from "class-validator";
+
 export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
     name?: string;
+
+    @IsOptional()
+    @IsString()
     lastname?: string;
+
+    @IsOptional()
+    @IsString()
     career?: string;
+
+    @IsOptional()
+    @IsString()
     reference_zone?: string;
+
+    @IsOptional()
+    @IsString()
     phone?: string;
+
+    @IsOptional()
+    @IsString()
     image?: string;
+
+    @IsOptional()
+    @IsString()
     notification_token?: string;
+
+    @IsOptional()
+    @IsBoolean()
     is_suspended?: boolean;
+
+    @IsOptional()
+    @IsDateString()
     suspended_until?: Date;
 }
